8f17f673f2fc3c313d255ea0120eb01cf24eb9a8
julymeltdown <quintuplets2000@gmail.com>
2023-07-19 16:33:12 +0900

    [BE FIX] 오류 수정

--- sns/src/main/kotlin/com/sconnect/sns/repository/LikeRepository.kt
+++ sns/src/main/kotlin/com/sconnect/sns/repository/LikeRepository.kt
@@ -6,5 +6,4 @@ import org.springframework.data.jpa.repository.JpaRepository
 
 interface LikeRepository:JpaRepository<Like,Long> {
     fun findByPostIdAndUserId(postId: Long, userId: Long)
-    fun
 }
